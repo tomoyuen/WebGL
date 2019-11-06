55c84aea90fbd8b873f54295eb7e631567b3652e
tomoyuen <wisyue@yahoo.com>
2019-11-06 15:46:44 +0800

    optimize code

--- MDN.js
+++ MDN.js
@@ -1,11 +1,6 @@
-// Shared code for the examples
-
-// Define the MDN global
-var MDN = {};
-
 // Define the data that is needed to make a 3d cube
-MDN.createCubeData = function() {
-  var positions = [
+function createCubeData() {
+  const positions = [
     // Front face
     -1.0, -1.0,  1.0,
     1.0, -1.0,  1.0,
@@ -43,7 +38,7 @@ MDN.createCubeData = function() {
     -1.0,  1.0, -1.0
   ];
 
-  var colorsOfFaces = [
+  const colorsOfFaces = [
     [0.3, 1.0, 1.0, 1.0], // Front face: cyan
     [1.0, 0.3, 0.3, 1.0], // Back face: red
     [0.3, 1.0, 0.3, 1.0], // Top face: green
@@ -52,17 +47,17 @@ MDN.createCubeData = function() {
     [1.0, 0.3, 1.0, 1.0] // Left face: purple
   ];
 
-  var colors = [];
+  let colors = [];
 
   for (var j = 0; j < 6; j++) {
-    var polygonColor = colorsOfFaces[j];
+    let polygonColor = colorsOfFaces[j];
 
-    for (var i = 0; i < 4; i++) {
+    for (let i = 0; i < 4; i++) {
       colors = colors.concat(polygonColor);
     }
   }
 
-  var elements = [
+  const elements = [
     0,  1,  2,      0,  2,  3,    // front
     4,  5,  6,      4,  6,  7,    // back
     8,  9,  10,     8,  10, 11,   // top
@@ -79,16 +74,16 @@ MDN.createCubeData = function() {
 }
 
 // Take the data for a cube and bind the buffers for it. Return an object collection of the buffers
-MDN.createBuffersForCube = function(gl, cube) {
-  var positions = gl.createBuffer();
+function createBuffersForCube(gl, cube) {
+  const positions = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positions);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cube.positions), gl.STATIC_DRAW);
 
-  var colors = gl.createBuffer();
+  const colors = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, colors);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cube.colors), gl.STATIC_DRAW);
 
-  var elements = gl.createBuffer();
+  const elements = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elements);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cube.elements), gl.STATIC_DRAW);
 
@@ -99,14 +94,14 @@ MDN.createBuffersForCube = function(gl, cube) {
   }
 }
 
-MDN.matrixArrayToCssMatrix = function(array) {
+function matrixArrayToCssMatrix(array) {
   return `matrix3d(${array.join(',')})`;
 }
 
-MDN.multiplyPoint = function(matrix, point) {
-  var x = point[0], y = point[1], z = point[2], w = point[3];
+function multiplyPoint(matrix, point) {
+  const x = point[0], y = point[1], z = point[2], w = point[3];
 
-  var c1r1 = matrix[ 0], c2r1 = matrix[ 1], c3r1 = matrix[ 2], c4r1 = matrix[ 3],
+  const c1r1 = matrix[ 0], c2r1 = matrix[ 1], c3r1 = matrix[ 2], c4r1 = matrix[ 3],
       c1r2 = matrix[ 4], c2r2 = matrix[ 5], c3r2 = matrix[ 6], c4r2 = matrix[ 7],
       c1r3 = matrix[ 8], c2r3 = matrix[ 9], c3r3 = matrix[10], c4r3 = matrix[11],
       c1r4 = matrix[12], c2r4 = matrix[13], c3r4 = matrix[14], c4r4 = matrix[15];
@@ -119,18 +114,18 @@ MDN.multiplyPoint = function(matrix, point) {
   ];
 }
 
-MDN.multiplyMatrices = function(a, b) {
+function multiplyMatrices(a, b) {
   // currently taken from https://github.com/toji/gl-matrix/blob/master/src/gl-matrix/mat4.js#L306-L337
 
-  var result = [];
+  const result = [];
 
-  var a00 = a[0], a01 = a[1], a02 = a[2], a03 = a[3],
+  const a00 = a[0], a01 = a[1], a02 = a[2], a03 = a[3],
       a10 = a[4], a11 = a[5], a12 = a[6], a13 = a[7],
       a20 = a[8], a21 = a[9], a22 = a[10], a23 = a[11],
       a30 = a[12], a31 = a[13], a32 = a[14], a33 = a[15];
 
   // Cache only the current line of the second matrix
-  var b0  = b[0], b1 = b[1], b2 = b[2], b3 = b[3];
+  let b0  = b[0], b1 = b[1], b2 = b[2], b3 = b[3];
   result[0] = b0*a00 + b1*a10 + b2*a20 + b3*a30;
   result[1] = b0*a01 + b1*a11 + b2*a21 + b3*a31;
   result[2] = b0*a02 + b1*a12 + b2*a22 + b3*a32;
@@ -157,19 +152,18 @@ MDN.multiplyMatrices = function(a, b) {
   return result;
 }
 
-MDN.multiplyArrayOfMatrices = function(matrices) {
-  var inputMatrix = matrices[0];
+function multiplyArrayOfMatrices(matrices) {
+  let inputMatrix = matrices[0];
 
-  for (let i = 0; i < matrices.length; i++) {
-    inputMatrix = MDN.multiplyMatrices(inputMatrix, matrices[i]);
+  for (let i = 1; i < matrices.length; i++) {
+    inputMatrix = multiplyMatrices(inputMatrix, matrices[i]);
   }
 
   return inputMatrix;
 }
 
-MDN.rotateXMatrix = function(a) {
-  var cos = Math.cos;
-  var sin = Math.sin;
+function rotateXMatrix(a) {
+  const { cos, sin } = Math;
 
   return [
     1,      0,       0, 0,
@@ -179,9 +173,8 @@ MDN.rotateXMatrix = function(a) {
   ];
 }
 
-MDN.rotateYMatrix = function(a) {
-  var cos = Math.cos;
-  var sin = Math.sin;
+function rotateYMatrix(a) {
+  const { cos, sin } = Math;
 
   return [
       cos(a), 0, sin(a), 0,
@@ -191,9 +184,8 @@ MDN.rotateYMatrix = function(a) {
   ];
 }
 
-MDN.rotateZMatrix = function(a) {
-  var cos = Math.cos;
-  var sin = Math.sin;
+function rotateZMatrix(a) {
+  const { cos, sin } = Math;
 
   return [
     cos(a), -sin(a), 0, 0,
@@ -203,7 +195,7 @@ MDN.rotateZMatrix = function(a) {
   ];
 }
 
-MDN.translateMatrix = function(x, y, z) {
+function translateMatrix(x, y, z) {
   return [
     1, 0, 0, 0,
     0, 1, 0, 0,
@@ -212,7 +204,7 @@ MDN.translateMatrix = function(x, y, z) {
   ];
 }
 
-MDN.scaleMatrix = function(w, h, d) {
+function scaleMatrix(w, h, d) {
   return [
     w, 0, 0, 0,
     0, h, 0, 0,
@@ -221,18 +213,18 @@ MDN.scaleMatrix = function(w, h, d) {
   ];
 }
 
-MDN.perspectiveMatrix = function(fieldOfViewInRadians, aspectRatio, near, far) {
+function perspectiveMatrix(fieldOfViewInRadians, aspectRatio, near, far) {
   // Construct a perspective matrix
 
   /*
-    Field of view - the angle in radians of waht's in view along the Y axis
+    Field of view - the angle in radians of what's in view along the Y axis
     Aspect Ratio - the ratio of the canvas, typically canvas.width / canvas.height
     Near - anything before this point in the Z direction gets clipped (outside of the clip space)
     Far - anything after this point in the Z direction gets clipped (outside of the clip space)
   */
 
-  var f = 1.0 / Math.tan(fieldOfViewInRadians / 2);
-  var rangeInv = 1 / (near - far);
+  const f = 1.0 / Math.tan(fieldOfViewInRadians / 2);
+  const rangeInv = 1 / (near - far);
 
   return [
     f / aspectRatio, 0, 0, 0,
@@ -242,16 +234,16 @@ MDN.perspectiveMatrix = function(fieldOfViewInRadians, aspectRatio, near, far) {
   ];
 }
 
-MDN.orthographicMatrix = function(left, right, bottom, top, near, far) {
+function orthographicMatrix(left, right, bottom, top, near, far) {
   // Each of the parameters repersents the plane of the bounding box
 
-  var lr = 1 / (left - right);
-  var bt = 1 / (bottom - top);
-  var nf = 1 / (near - far);
+  const lr = 1 / (left - right);
+  const bt = 1 / (bottom - top);
+  const nf = 1 / (near - far);
 
-  var row4col1 = (left + right) * lr;
-  var row4col2 = (top + bottom) * bt;
-  var row4col3 = (far + near) * nf;
+  const row4col1 = (left + right) * lr;
+  const row4col2 = (top + bottom) * bt;
+  const row4col3 = (far + near) * nf;
 
   return [
     -2 * lr, 0, 0, 0,
@@ -261,23 +253,22 @@ MDN.orthographicMatrix = function(left, right, bottom, top, near, far) {
   ];
 }
 
-MDN.createShader = function(gl, source, type) {
+function createShader(gl, source, type) {
   // Compiles either a shader of type gl.VERTEX_SHADER or gl.FRAGMENT_SHADER
 
-  var shader = gl.createShader(type);
+  const shader = gl.createShader(type);
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
 
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    var info = gl.getShaderInfoLog(shader);
-    throw "Could not compile WebGL program. \n" + info;
+    throw `Could not compile WebGL program. \n ${gl.getShaderInfoLog(shader)}`;
   }
 
   return shader;
 }
 
-MDN.linkProgram = function(gl, vertexShader, fragmentShader) {
-  var program = gl.createProgram();
+function linkProgram(gl, vertexShader, fragmentShader) {
+  const program = gl.createProgram();
 
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
@@ -285,31 +276,30 @@ MDN.linkProgram = function(gl, vertexShader, fragmentShader) {
   gl.linkProgram(program);
 
   if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    var info = gl.getProgramInfoLog(program);
-    throw "Could not compile WebGL program. \n" + info;
+    throw `Could not compile WebGL program. \n ${gl.getProgramInfoLog(program)}`;
   }
 
   return program;
 }
 
-MDN.createWebGLProgram = function(gl, vertexSource, fragmentSource) {
-  // combine MDN.createShader() and MDN.linkProgram()
+function createWebGLProgram(gl, vertexSource, fragmentSource) {
+  // combine createShader() and linkProgram()
 
-  var vertexShader = MDN.createShader(gl, vertexSource, gl.VERTEX_SHADER);
-  var fragmentShader = MDN.createShader(gl, fragmentSource, gl.FRAGMENT_SHADER);
+  const vertexShader = createShader(gl, vertexSource, gl.VERTEX_SHADER);
+  const fragmentShader = createShader(gl, fragmentSource, gl.FRAGMENT_SHADER);
 
-  return MDN.linkProgram(gl, vertexShader, fragmentShader);
+  return linkProgram(gl, vertexShader, fragmentShader);
 }
 
-MDN.createWebGLProgramFromIds = function(gl, vertexSourceId, fragmentSourceId) {
-  var vertexSourceEl = document.getElementById(vertexSourceId);
-  var fragmentSourceEl = document.getElementById(fragmentSourceId);
+function createWebGLProgramFromIds(gl, vertexSourceId, fragmentSourceId) {
+  const vertexSourceEl = document.getElementById(vertexSourceId);
+  const fragmentSourceEl = document.getElementById(fragmentSourceId);
 
-  return MDN.createWebGLProgram(gl, vertexSourceEl.innerHTML, fragmentSourceEl.innerHTML);
+  return createWebGLProgram(gl, vertexSourceEl.innerHTML, fragmentSourceEl.innerHTML);
 }
 
-MDN.createContext = function(canvas) {
-  var gl;
+function createContext(canvas) {
+  let gl;
 
   try {
     // try to grab the standard context. If it fails, fallback to experimental.
@@ -317,7 +307,7 @@ MDN.createContext = function(canvas) {
   } catch(e) {}
 
   if (!gl) {
-    var message = 'Unable to initialize WebGL. Your browser may not support it.';
+    const message = 'Unable to initialize WebGL. Your browser may not support it.';
     alert(message);
     throw new Error(message);
     gl = null;
@@ -325,3 +315,21 @@ MDN.createContext = function(canvas) {
 
   return gl;
 }
+
+export default {
+  createCubeData,
+  createBuffersForCube,
+  matrixArrayToCssMatrix,
+  multiplyPoint,
+  multiplyMatrices,
+  multiplyArrayOfMatrices,
+  rotateXMatrix,
+  rotateYMatrix,
+  rotateZMatrix,
+  translateMatrix,
+  scaleMatrix,
+  perspectiveMatrix,
+  orthographicMatrix,
+  createWebGLProgramFromIds,
+  createContext
+};
